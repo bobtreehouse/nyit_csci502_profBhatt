afe3cc19668a1cefd396e77e2460c00c9bd7ea9c
bobtrieste <bobtrieste@gmail.com>
2020-10-05 20:06:16 -0400

    Updated with samples of Loops

---
+++ JavaLabeledForLoop/src/JavaLabeledForLoop.java
@@ -0,0 +1,9 @@
+
+public class JavaLabeledForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ WhileLoop/src/WhileLoop.java
@@ -0,0 +1,18 @@
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i<=10) {
+			System.out.println(i);
+			i++; /*without this statement to the left we would have an 
+			infinite loop b/c i would never get up to above 10*/
+			
+		 
+			
+		}
+
+	}
+
+}
