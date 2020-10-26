0b799c1dd51c27bebb2e806ea68f9781b3f945ef
Bob Trieste <bob@fivetreecapital.com>
2020-10-26 07:16:26 -0400

    added min Number method example

---
+++ MinMaxAvg/src/MinMaxAvg.java
@@ -0,0 +1,56 @@
+ 
+
+import java.util.Scanner;
+
+public class MinMaxAvg {
+
+    public static void main(String[] args) {
+
+        // Set up the scanner and variables
+        Scanner scanner = new Scanner(System.in);
+        int size = 10;
+        int[] numbers = new int[size];
+        String stringIn;
+        int min;
+        int max;
+        int sum = 0;
+        double avg;
+
+        // Read in 10 ints
+        for (int i = 0; i < numbers.length; i++) {
+
+            System.out.println("Please enter a number:");
+            stringIn = scanner.nextLine();
+            numbers[i] = Integer.parseInt(stringIn);
+        }
+
+        // Calculate min
+        min = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        // Calculate max
+        max = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        // Calculate average
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        avg = sum/(double)numbers.length;
+
+        System.out.println("Minimum value: " + min);
+        System.out.println("Maximum value: " + max);
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + avg);
+    }
+
+}
