cbf3bc78d70d2c53baa1f4b52f0ca3e9b7ad83ca
Bob Trieste <bob@fivetreecapital.com>
2020-11-02 15:48:16 -0500

    added Switch Example and Reverse Int Array

---
+++ PreOrPostUnary/src/PreOrPortUnary.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class PreOrPortUnary {
+
+		    public static void main(String[] args) 
+		    { 
+		        // initializing variables 
+		        int num = 5; 
+		  
+		    
+		        
+		        // first 5 gets printed and then 
+		        // increment to 6 
+		        System.out.println("Post "
+		                           + "increment = " + num++); 
+		  
+		        // num was 6, incremented to 7 
+		        // then printed 
+		        System.out.println("Pre "
+		                           + "increment = " + ++num); 
+		    } 
+		
+	}
+
+
